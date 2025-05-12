5a1552b21bf2c26694ff8964b7d7c71aafecd445
zolboo-gtn <72186937+zolboo-gtn@users.noreply.github.com>
2025-05-12 17:03:51 +0800

    misc

--- src/mastra/index.ts
+++ src/mastra/index.ts
@@ -2,11 +2,11 @@ import { Mastra } from "@mastra/core/mastra";
 // import { createLogger } from "@mastra/core/logger";
 
 import { burnoutTestAgent } from "./agents/burnout_test";
-import { postgresStorage } from "./storage";
+// import { postgresStorage } from "./storage";
 
 export const mastra = new Mastra({
   agents: {
     burnoutTest: burnoutTestAgent,
   },
-  storage: postgresStorage,
+  // storage: postgresStorage,
 });
